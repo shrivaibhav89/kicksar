91983e9cacec2ce1fafa20c8aa874a2fe6d788a6
LAPTOP-DG0KG3M0\anubhav <shhrivastava.vaibhav7@gmail.com>
2021-09-02 22:59:35 +0530

    build added

---
+++ Assets/Scripts/navigation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class navigation : MonoBehaviour
+{
+    public void LoadScene(string scname)
+    {
+        SceneManager.LoadScene(scname);
+    }
+}
